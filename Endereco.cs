44821ca6e5b6cfb8a963fa5058e154e077cd2908
Daniel Carvalho Climaco <111577285+DClimaco@users.noreply.github.com>
2024-02-22 08:35:24 -0300

    Add files via upload

---
+++ Endereco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20240222
+{
+    internal class Endereco
+    {
+        public string rua;
+        public double numero;
+        public Endereco(string rua, double numero)
+        {
+            this.rua = rua;
+            this.numero = numero;
+        }
+    }
+}
---
+++ Proprietario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20240222
+{
+    internal class Proprietario
+    {
+        private string nome;
+        private string cpf;
+        private Endereco endereco;
+
+    }
+}
